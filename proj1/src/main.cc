358741bd65e3a51267efe95e9c13db59d695d8db
CSCE-JerardA <jerardaustin4@gmail.com>
2026-01-27 14:26:50 -0500

    Initial commit: I am still coding within the main file

--- proj1/src/main.cc
+++ proj1/src/main.cc
@@ -1,10 +1,32 @@
 // Copyright Jerard Austin 2026
 
+
 #include <iostream>
-#include <pthread.h>
+#include <proj1/include/main.h> // Used to 
+#include <pthread.h>  // For my use of threads
 #include <vector>
 #include <proj1/lib/timings.h>
+#include <string>
+#include <fstream>  // For handling my files
 
 using namespace std;
+
+// Structure for my threads
+struct ThreadsArg {
+    int index;
+    int k;
+    bool* released_flags;
+
+    std::string plan;
+    const std::vector<std::string>* file_data;
+
+};
+
+
+void ThreadRoutine (void* arg) {
+    // Statement for processing my rows
+    return nullptr;
+}
+
 
 

864e34f8534e561eea1de869140bb1e72b37308b
CSCE-JerardA <jerardaustin4@gmail.com>
2026-01-29 00:08:01 -0500

    Finsihed working on my routine function

--- proj1/src/main.cc
+++ proj1/src/main.cc
@@ -1,6 +1,6 @@
 // Copyright Jerard Austin 2026
 
-
+#include <unistd.h>
 #include <iostream>
 #include <pthread.h>  // For my use of threads
 #include <vector>
@@ -8,7 +8,7 @@
 #include <fstream>  // For handling my files
 
 // For the provided libraries
-#include <proj1/lib/timings.h> // Used for 
+#include <proj1/lib/timings.h> 
 #include <proj1/lib/cli_parser.h>
 #include <proj1/lib/thread_log.h>
 #include <proj1/lib/sha256.h>
@@ -18,38 +18,76 @@ using namespace std;
 
 // Structure for the data of my threads
 struct ThreadsArg {
-    int index;
-    int k;
-    bool* released_flags;
+    int index;  // The ID
+    int k;  // The active threads
+    bool* released_flags;  // Pointer for flagged array
 
-    std::string plan;
-    const std::vector<std::string>* file_data;
+    CliMode mode;  // Strategy for release
+    const std::vector<std::string>* fdata;  // Pointer to the file data
 
 };
 
-
+// Function for what the pthreads execute
 void ThreadRoutine (void* arg) {
-    // Statement for processing my rows
+    
+    //
+    ThreadsArg* data = static_cast<ThreadsArg*>(arg);
+
+    // My local variables for connecting my variables through member access operators (arrows)
+    // Used cppreference for this part to understand what I was doing
+    int my_id = data->index;
+    int limit = data->k;
+    bool* flags = data->released_flags;
+    CliMode strat = data->mode;
+    
+
+
+    // Waits until released
+    while (!flags[limit]) {
+        Timings_SleepMs(1);
+    }
+
+    // If the index is higher than k it exits immediately
+    if (my_id > limit) {
+        return nullptr;
+    }
+
+    // Goes from thread to thread
+    if (strat == CLI_MODE_THREAD && my_id < limit) {
+        flags[limit + 1] = true;
+    }
+
+    ThreadLog("[thread %d] started", my_id);
+
+    // Local variable for file data pointer
+    const auto& rows = *(data->fdata);
+
+    for (int m = 0; ; m++) {
+
+        int row_ind = my_id * m;
+
+        // Statement that checks if we're out of rows
+        if (row_ind >= rows.size()) {
+            break;
+        }
+
+        char results[65]; // Turns the data into a hexademical 
+        // Becoming 64 characters long w/ a null terminator at the end
+
+        ComputeIterativeSha256Hex(uint8_t*)rows[row_ind].c_str(), rows[row_ind].length(), 100000, results;
+
+        ThreadLog("Thread %d processing row %d", my_id, row_ind);
+    }
+
+    ThreadLog("Thread %d returned ", my_id);
     return nullptr;
+
 }
 
 
 int main (int argc, char* argv[]) {
 
-    //
-    ThreadsArg* data = static_cast<ThreadsArg*>(arg)
 
-    // Statement that makes 
-    while (!data->released_flags[data->index]) {
-        Timings_SleepMs(1);
-    }
-
-    // If the index is higher than k it exits immediately
-    if (data->index > data->k) {
-        return nullptr;
-    }
-
-    
 
 
 }
